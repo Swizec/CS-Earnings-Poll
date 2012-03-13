14e95749f6276c16bcd21fec35f9be7af5589bae
Swizec Teller <swizec@swizec.com>
2012-03-14 00:01:27 +0100

    pies change when data sliced differently

--- js/main.js
+++ js/main.js
@@ -4,13 +4,29 @@
     var pie = gief_piechart('#pie1', "students");
     var pie2 = gief_piechart('#pie2', "students");
 
+    var filter_functions = {
+        students: function (item) { return !!item.cs_student; },
+        others: function (item) { return !item.cs_student; },
+        everyone: function (item) { return true; }
+    },
+        filter_func = filter_functions.students,
+        slider_value = 1;
+
+    var update = function () {
+        pie(year_vs_pay(slider_value,
+                        filter_func));
+        pie2(year_vs_pay(slider_value-1,
+                         filter_func));
+    };
+
     $(".btn-group .btn").addClass("btn-primary");
     $(".btn-group .default").button('toggle');
 
     $(".btn-group .btn").click(function (e) {
         var $btn = $(e.currentTarget);
         if (!$btn.hasClass('active')) {
-            console.log($btn.attr('name'));
+            filter_func = filter_functions[$btn.attr('name')];
+            update();
         }
     });
 
@@ -21,22 +37,22 @@
 	step: 1,
         width: '100px',
 	slide: function( event, ui ) {
-	    $( "#amount" ).html("Studying for "+ui.value+" years");
-            pie(year_vs_pay(ui.value), "students");
-            pie2(year_vs_pay(ui.value-1), "students");
+            slider_value = ui.value;
+            $("#amount").html("Studying for "+slider_value+" years.");
+            update();
 	}
     });
-    pie(year_vs_pay(1));
-    pie(year_vs_pay(1));
+    pie(year_vs_pay(1, filter_func));
+    pie(year_vs_pay(1, filter_func));
 
-    pie2(year_vs_pay(3));
-    pie2(year_vs_pay(3));
+    pie2(year_vs_pay(3, filter_func));
+    pie2(year_vs_pay(3, filter_func));
 
 })(jQuery);
 
 /// DATA FUNCTIONS
 
-function year_vs_pay(year) {
+function year_vs_pay(year, subset) {
     var hourly_rate_labels = ['do 7€/h',
                               '7€/h do 13€/h',
                               '13€/h do 16€/h',
@@ -45,7 +61,7 @@ function year_vs_pay(year) {
                               '50€/h do 80€/h',
                               'nad 80€/h'];
 
-    var data = DATA.filter(function (item) { return item.years_study == year; }),
+    var data = DATA.filter(subset).filter(function (item) { return item.years_study == year; }),
         fin_data = d3.range(7).map(function (i) {
             return {label: hourly_rate_labels[i],
                     count: 0};
